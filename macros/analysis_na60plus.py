7b4f5e17e185bc08c193af6563ad7393369c3f3b
Giacomo Alocco <giacomo.alocco@cern.ch>
2023-11-03 15:33:03 +0100

    fix vertexing

--- macros/analysis_na60plus.py
+++ macros/analysis_na60plus.py
@@ -1,155 +1,181 @@
-from ROOT import TH1D,TH2D, TFile, TF2, TCanvas, kRed, TLegend, TFile, gStyle, kRainBow
+from ROOT import TH1D,TH2D, TFile, TF2, TCanvas, kRed, TLegend, TFile, gStyle, kRainBow, TTree
 import uproot
 import numpy as np
 import math
 import argparse
+import vertexer
+import pandas as pd
+from array import array
+import yaml
+import os
 
-def read_tree():
+def read_tree(path, compute_sigmas = False, config = "", nsigma = 1, suffix =""):
     #put here the path of the file you want to analyze
-    #path = "/home/giacomo/its-corryvreckan-tools/output/SPSNovember22/alignment_475230516221125231020.root"
-    path = "/Users/lucamicheletti/its-corryvreckan-tools/output/SPSNovember22/alignment_475230516221125231020.root"
-
     df_beam = uproot.open(path)['Tracking4D/tree/event/beam'].arrays(library="pd")
     df_tracks = uproot.open(path)['Tracking4D/tree/event/tracks'].arrays(library="pd")
 
-    th1vxy = TH2D("tracks_vxy",";;",1000,-0.1,0.1,1000,-0.1,0.1)
-    th1pxy = TH2D("tracks_pxy","track position at z = z_target; x (mm); y(mm)",2000,-5,5,2000,-5,5)
-    th1res_pxy = TH2D("res_tracks_pxy","track position - Pb position at z = z_target; x_{trk}-x_{Pb} (mm); y_{trk}-y_{Pb} (mm)",2000,-5,5,2000,-5,5)
-    th1res_pxy_sel = TH2D("res_tracks_pxy_sel","track position - Pb position at z = z_target; x_{trk}-x_{Pb} (mm); y_{trk}-y_{Pb} (mm)",2000,-5,5,2000,-5,5)
-    th2_eta_vs_mult = TH2D("eta_vs_mult",";#eta;multiplicity;counts",100,0,10,400,-0.5,399.5)
-    th2_eta_vs_mult_sel = TH2D("eta_vs_mult_sel",";#eta;multiplicity;counts",100,0,10,400,-0.5,399.5)
-    th1_multi = TH1D("multiplicity","multiplicity;number of tracks per event;counts",400,-0.5,399.5)
-    th1_multi_sel = TH1D("multiplicity selected","multiplicity;number of tracks per event;counts",400,-0.5,399.5)
-    th1_eta = TH1D("eta","eta;#eta;counts",100,0,10)
-    th1_eta_sel = TH1D("eta_sel","eta;#eta;counts",100,0,10)
-    th1_mean_clsize = TH1D("th1_mean_clsize","clsize;<CL size>;counts",1000,0,100)
-    th1_mean_clsize_sel = TH1D("th1_mean_clsize_sel","clsize;<CL size>;counts",1000,0,100)
-    th2_eta_vs_mean_clsize = TH2D("eta_vs_mean_clsize",";#eta;<CL size>;counts",1000,0,10,1000,0,100)
-    th2_eta_vs_mean_clsize_sel = TH2D("eta_vs_mean_clsize_sel",";#eta;<CL size>;counts",1000,0,10,1000,0,100)
+    with open(os.path.expandvars(config), 'r') as stream:
+        try:
+            params = yaml.full_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    
+    target = params["TARGET"]
+    z_target = params["Z_TARGET"]
+    thickness = params["THICKNESS"]
+    interaction_prob = params["INTERACTION_PROB"]
 
-    #position of the target in the telescope
-    z_target = 125 #mm
+    sigma_x = params["SIGMA_X_Pb"]
+    sigma_y = params["SIGMA_Y_Pb"]
+    mean_x = params["MEAN_X_Pb"]
+    mean_y = params["MEAN_Y_Pb"]
+
+    sigma_x_vtx = params["SIGMA_X_VTX"]
+    sigma_y_vtx = params["SIGMA_Y_VTX"]
+    mean_x_vtx = params["MEAN_X_VTX"]
+    mean_y_vtx = params["MEAN_Y_VTX"]
+    
 
     old_index = 0
 
-    x0 = df_beam.at[df_beam.index[0],"beam.x"]
-    y0 = df_beam.at[df_beam.index[0],"beam.y"]
+    vx = df_beam.at[df_beam.index[0],"beam.vx"]
+    vy = df_beam.at[df_beam.index[0],"beam.vy"]
+    px = df_beam.at[df_beam.index[0],"beam.px"]
+    py = df_beam.at[df_beam.index[0],"beam.py"]
+
+
+    x0 = vx*z_target+px
+    y0 = vy*z_target+py
 
     ntracks = 0
     ntracks_sel = 0
 
-    sigma_x = 0.05292421774392007
-    sigma_y = 0.040370864336865826
-    mean_x = -0.015625873902035948
-    mean_y = -0.024083787738233064
+
     eta_list = []
     eta_list_sel = []
+    #Vertex QA plots
+    res_tracks_pb = TH2D("res_tracks_pb","track position - Pb position at z = z_target; x_{trk}-x_{Pb} (mm); y_{trk}-y_{Pb} (mm)",2000,-5,5,2000,-5,5)
+    res_tracks_pv = TH2D("res_tracks_pv","track position - PV position at z = z_target; x_{trk}-x_{V} (mm); y_{trk}-y_{V} (mm)",2000,-5,5,2000,-5,5)
+    hMultiplicity = TH1D("hMultiplicity",";track multiplicity;",500,0,499.5)
+    hMultiplicitySel = TH1D("hMultiplicitySel",";track multiplicity;",500,0,499.5)
+    hNcontr = TH1D("hNcontr",";number of contributors;",100,0,99.5)
+    hNcontrVsZ = TH2D("hNcontrVsZ",";number of contributors;z_{v} (mm]);",100,0,99.5,100,0,150)
+    hDeltaPbPVX = TH1D("hDeltaPbPVX",";x_{Pb}-x_{v} (mm);",1000,-0.5,0.5)
+    hDeltaPbPVY = TH1D("hDeltaPbPVY",";y_{Pb}-y_{v} (mm);",1000,-0.5,0.5)
+    hVertexX = TH1D("hVertexX",";x_{v} (mm);",100,-10,10)
+    hVertexY = TH1D("hVertexY",";y_{v} (mm);",100,-10,10)
+    hVertexZ = TH1D("hVertexZ",";z_{v} (mm);",1000,25,125)
+    hVertexZHighMult = TH1D("hVertexZHighMult",";z_{v} (mm);",1000,25,125)
+
+    #Vertexing
+    df_vtx = pd.DataFrame(columns=df_tracks.columns.tolist())
+    primary_vertexes = []
+    # position (xv,yv,zv), nummber of contributors
+    n_contributors = 0
+    for index, row in df_tracks.iterrows():
+        if old_index != index[0]:
+            vertex = vertexer.fit_primary_vertex(df_vtx, df_beam.iloc[old_index],z_target)
+            primary_vertexes.append([vertex[0],vertex[1],vertex[2],n_contributors])
+            df_vtx.drop(df_vtx.index, inplace=True)
+            hDeltaPbPVX.Fill(x0-vertex[0])
+            hDeltaPbPVY.Fill(y0-vertex[1])
+            old_index = index[0]
+            vx = df_beam.at[df_beam.index[old_index],"beam.vx"]
+            vy = df_beam.at[df_beam.index[old_index],"beam.vy"]
+            px = df_beam.at[df_beam.index[old_index],"beam.px"]
+            py = df_beam.at[df_beam.index[old_index],"beam.py"]
+            hVertexX.Fill(vertex[0])
+            hVertexY.Fill(vertex[1])
+            if n_contributors > 0:
+                hVertexZ.Fill(vertex[2])
+            if n_contributors > 20:
+                hVertexZHighMult.Fill(vertex[2])
+            hNcontr.Fill(n_contributors)
+            hNcontrVsZ.Fill(n_contributors, vertex[2])
+
+            n_contributors = 0
+            x0 = vx*z_target+px
+            y0 = vy*z_target+py
+        
+        x0 = vx*z_target+px
+        y0 = vy*z_target+py
+        x = row["tracks.px"]+z_target*row["tracks.vx"]
+        y = row["tracks.py"]+z_target*row["tracks.vy"]
+        dx = ((x-x0)-mean_x)/sigma_x
+        dy = ((y-y0)-mean_y)/sigma_y
+        res_tracks_pb.Fill(x-x0,y-y0)
+
+        if dx**2+dy**2 < nsigma**2:#selecting the tracks 5 sigma from the primary vertex
+            n_contributors += 1
+            #row_to_add = df_tracks.iloc[0]  # Select the first row from df1
+            df_vtx = df_vtx.append(row, ignore_index=True)
+
+    vertexer.define_PV_selection(res_tracks_pb, config, "Pb")
+    output = TFile("../results/output_na60plus_nsigma"+str(nsigma)+"_"+suffix+".root","recreate")
+    subdir_vtx = output.mkdir("vertex-qa")
+    subdir_vtx.cd()
+    res_tracks_pb.Write()
+    hDeltaPbPVX.Write()
+    hDeltaPbPVY.Write()
+    hVertexX.Write()
+    hVertexY.Write()
+    hVertexZ.Write()
+    hVertexZHighMult.Write()
+    hNcontr.Write()
+    hNcontrVsZ.Write()
+
+    subdir_mult = output.mkdir("multiplicity")
+    subdir_mult.cd()
+    #analysis
+    print(len(primary_vertexes))
+    old_index = 0
+    vertex = primary_vertexes[0]
+    x0 = vertex[0]
+    y0 = vertex[1]
+    z0 = vertex[2]
+    n_con = vertex[3]
+    counter = 0
     for index, row in df_tracks.iterrows():
         if old_index != index[0]:
             old_index = index[0]
+            counter += 1
+            if counter == len(primary_vertexes):
+                break
+            vertex = primary_vertexes[counter]
             #position of the Pb ion
-            x0 = df_beam.at[df_beam.index[old_index],"beam.x"]
-            y0 = df_beam.at[df_beam.index[old_index],"beam.y"]
-            th1_multi.Fill(ntracks)
-            th1_multi_sel.Fill(ntracks_sel)
-            for eta in eta_list:
-                th2_eta_vs_mult.Fill(eta, ntracks)
-            for eta in eta_list_sel:
-                th2_eta_vs_mult_sel.Fill(eta, ntracks_sel)
-            eta_list = []
-            eta_list_sel = []
+            x0 = vertex[0]
+            y0 = vertex[1]
+            z0 = vertex[2]
+
+            n_con = vertex[3]
+            #print(vertex)
+
             ntracks = 0
             ntracks_sel = 0
         ntracks += 1
-        #position of the track at z = z_target
-        x = row["tracks.px"]+z_target*row["tracks.vx"]
-        y = row["tracks.py"]+z_target*row["tracks.vy"]
 
-        clsize_list = np.array([row["tracks.size1"], row["tracks.size2"], row["tracks.size3"], row["tracks.size4"], row["tracks.size5"]])
+        #position of the track at z = z_target
+        x = row["tracks.px"]+z0*row["tracks.vx"]
+        y = row["tracks.py"]+z0*row["tracks.vy"]
+
+        clsize_list = np.array([row["tracks.size1"], row["tracks.size2"], row["tracks.size3"], row["tracks.size4"], row["tracks.size5"], row["tracks.size6"]])
         clsize_list[clsize_list != 0]
         mean_clsize = np.mean(clsize_list)
 
-        th1_mean_clsize.Fill(mean_clsize)
-        th1vxy.Fill(row["tracks.vx"],row["tracks.vy"])
-        th1pxy.Fill(x,y)
-        th1res_pxy.Fill(x-x0,y-y0)
-
-        dx = ((x-x0)-mean_x)/sigma_x
-        dy = ((y-y0)-mean_y)/sigma_y
-
-        p =  math.sqrt(row["tracks.vx"]**2+row["tracks.vy"]**2+1)
-        eta = math.log((p+1)/(p-1))/2.
-
-        th2_eta_vs_mean_clsize.Fill(eta,mean_clsize)
-
-        if dx**2+dy**2 < 5**2:#selecting the tracks 5 sigma from the primary vertex
-            ntracks_sel += 1
-            th1_eta_sel.Fill(eta)
-            eta_list_sel.append(eta)
-            th1res_pxy_sel.Fill(x-x0,y-y0)
-            th1_mean_clsize_sel.Fill(mean_clsize)
-            th2_eta_vs_mean_clsize_sel.Fill(eta,mean_clsize)
-
-        th1_eta.Fill(eta)
-        eta_list.append(eta)
-
-    tf2 = TF2("xygaus","xygaus",-0.25,0.25,-0.2,0.2)
-    th1res_pxy.Fit(tf2,"MR0")
-
-    sigma_x = tf2.GetParameter(2)
-    sigma_y = tf2.GetParameter(4)
-    mean_x = tf2.GetParameter(1)
-    mean_y = tf2.GetParameter(3)
-
-    #values used before to reject the tracks far from the primary vertex
-    print("sigma_x: ",sigma_x)
-    print("sigma_y: ",sigma_y)
-    print("mean_x: ",mean_x)
-    print("mean_y: ",mean_y)
+        dx = ((x-x0)-mean_x)/sigma_x_vtx
+        dy = ((y-y0)-mean_y)/sigma_y_vtx
 
 
-    #first estimation of the eta in the 0-5% centrality class
-    #number of events in the run
-    corry_output = TFile(path,"read") 
-    n_events = corry_output.Get("EventLoaderEUDAQ2/ALPIDE_1/hPixelMultiplicityPerCorryEvent").GetEntries()
-    #interaction probability of the beam with the target
-    pint = 0.15
-    #centrality class
-    centrality = [0.0,0.05]
-    #expected number of events
-    n_ev = (centrality[1]-centrality[0])*pint*n_events 
-    #select the bin after which we have the x most central event 
-    integral = 0
-    bin_min = 0
-    for i in range(th1_multi_sel.GetNbinsX(), 0, -1):
-        integral += th1_multi_sel.GetBinContent(i)
-        if integral > n_ev:
-            bin_min = i
-            break
+        eta = math.atanh(1./math.sqrt(row["tracks.vx"]**2+row["tracks.vy"]**2+1))
 
-    nbins = th2_eta_vs_mult_sel.GetNbinsY()
-    proj_eta = th2_eta_vs_mult_sel.ProjectionX("central_eta",bin_min, nbins)
-
-    #save the results
-    output = TFile("../results/output_na60plus.root","recreate")
-    th1vxy.Write()
-    th1pxy.Write()
-    proj_eta.Write()
-    th1res_pxy.Write()
-    th1res_pxy_sel.Write()
-    th1_mean_clsize.Write()
-    th1_mean_clsize_sel.Write()
-    th1_multi.Write()
-    th1_multi_sel.Write()
-    th1_eta.Write()
-    th1_eta_sel.Write()
-    th2_eta_vs_mult_sel.Write()
-    th2_eta_vs_mult.Write()
-    th2_eta_vs_mean_clsize.Write()
-    th2_eta_vs_mean_clsize_sel.Write()
+        #if dx**2+dy**2 < 5**2: #selecting the tracks 5 sigma from the primary vertex
+        ntracks_sel += 1
+        #if n_con > 0:
+        res_tracks_pv.Fill(x-x0, y-y0)
+    
+    vertexer.define_PV_selection(res_tracks_pv, config, "VTX")
+    res_tracks_pv.Write()
     output.Close()
-
-    exit()
 
 def plot_results():
     fIn = TFile("../results/output_na60plus.root")
@@ -216,11 +242,20 @@ def plot_results():
 def main():
     parser = argparse.ArgumentParser(description='Arguments to pass')
     parser.add_argument("--read_tree", help="Read data and produce output histograms", action="store_true")
+    parser.add_argument("--update", help="Update sigmas for PV selection", action="store_true")
     parser.add_argument("--plot_results", help="Produce plot from the analysis", action="store_true")
+    parser.add_argument("config", help="Path to the YAML configuration file")
     args = parser.parse_args()
 
+
     if args.read_tree:
-        read_tree()
+        sigma_list = [3]
+        for sigma in sigma_list:
+            path_new = "/home/giacomo/its-corryvreckan-tools/output/2023-10_SPS/Target/analysis_422213212231017213217_Ag_planes_6.root"
+            path_old = "/home/giacomo/its-corryvreckan-tools/output/2023-10_SPS/Target/analysis_422191053231017193343_Pb_planes_6.root"
+            path_be = "/home/giacomo/its-corryvreckan-tools/output/2023-10_SPS/Target/analysis_422231249231017231254_Be_planes_6.root"
+            read_tree(path= path_be,compute_sigmas=args.update,config=args.config,nsigma=sigma, suffix="422213212231017213217_Be_planes_6")
+
     if args.plot_results:
         plot_results()
 
